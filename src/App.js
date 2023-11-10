65fd79bc2d0d3fece2924b78f30709a6b7cf655e
Ansuman Singh <ansumansingh@Ansumans-MacBook-Pro.local>
2023-11-10 13:54:24 -0600

    react-mysite

--- src/App.js
+++ src/App.js
@@ -7,16 +7,11 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello Ansuman <br/>
+          || Be Happy <br/>
+          || Shri Radha Naam <br/>
+          || Shri Guru paduka namah!<br/>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
